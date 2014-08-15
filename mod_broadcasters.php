7dea6acaf8b0c3c412028c02071e46ce710079f3
Thiago Cesar <tcesarpinho@gmail.com>
2014-08-15 14:49:44 -0300

    Get Horarios

--- mod_broadcasters.php
+++ mod_broadcasters.php
@@ -1,16 +1,31 @@
-<?php
-/**
- * @copyright	@copyright	Copyright (c) 2014 Ponto Mega (http://pontomega.com.br). All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-// no direct access
-defined('_JEXEC') or die;
-
-// include the syndicate functions only once
-require_once dirname(__FILE__) . '/helper.php';
-
-
-$class_sfx = htmlspecialchars($params->get('class_sfx'));
-
+<?php
+
+/**
+
+ * @copyright	@copyright	Copyright (c) 2014 Ponto Mega (http://pontomega.com.br). All rights reserved.
+
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+
+ */
+
+
+
+// no direct access
+
+defined('_JEXEC') or die;
+
+
+
+// include the syndicate functions only once
+
+require_once dirname(__FILE__) . '/helper.php';
+
+
+
+
+
+$class_sfx = htmlspecialchars($params->get('class_sfx'));
+
+$diasdasemana = json_decode($params->get('horarios'));
+
 require(JModuleHelper::getLayoutPath('mod_broadcasters', $params->get('layout', 'default')));
