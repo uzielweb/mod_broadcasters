ce73fa3b6a6b5ca4452eabf524d379d89f1f1815
uzielweb <uzielweb@gmail.com>
2014-08-27 18:20:47 -0300

    Editado por Júlio Pontes
    Ajustes feitos por Júlio Pontes para exibir corretamente os itens.

--- tmpl/default.php
+++ tmpl/default.php
@@ -2,9 +2,9 @@
 
 /**
 
- * @copyright	Copyright (c) 2014 Ponto Mega (http://pontomega.com.br). All rights reserved.
+ * @copyright    Copyright (c) 2014 Ponto Mega (http://pontomega.com.br). All rights reserved.
 
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @license        GNU General Public License version 2 or later; see LICENSE.txt
 
  */
 
@@ -14,6 +14,13 @@
 
 defined('_JEXEC') or die;
 
+$rows = array();
+foreach ($diasdasemana as $diadasemana) {
+    foreach ($diadasemana as $key => $value) {
+        $rows[$key][] = $value;
+    }
+}
+
 
 
 JHtml::_('script', 'mod_broadcasters/script.js', array(), true);
@@ -22,15 +29,12 @@ JHtml::_('stylesheet', 'mod_broadcasters/style.css', array(), true);
 
 ?>
  <ul>
-<li>
- <?php
-  
-foreach ($diasdasemana as $diasdasemana)
-{
- echo $diasdasemana;
-}
- ?>
- 
-</li> 
- 
+     <?php foreach ($rows as $row): ?>
+    <li>
+        <?php echo $row[0]; ?> de <?php echo $row[1]; ?> as <?php echo $row[2]; ?> - <?php echo $row[3]; ?>
+        <?php if (!empty($row[4])): ?>
+        com <?php echo $row[4]; ?>
+        <?php endif; ?>
+    </li> 
+     <?php endforeach; ?>
  </ul>
