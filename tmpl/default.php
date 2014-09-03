16f689d9fc615b204e79c68cc5a6d32bdb3dfaf6
uzielweb <uzielweb@gmail.com>
2014-09-03 02:07:06 -0300

    Finalização
    Finalização da versão 1.0.0

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,40 +1,45 @@
 <?php
-
 /**
-
  * @copyright	Copyright (c) 2014 Ponto Mega (http://pontomega.com.br). All rights reserved.
-
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
-
  */
-
-
-
 // no direct access
-
 defined('_JEXEC') or die;
-
 $rows = array();
 foreach ($diasdasemana as $diadasemana) {
 	foreach ($diadasemana as $key => $value) {
 		$rows[$key][] = $value;
 	}
 }
-
-
-
 JHtml::_('script', 'mod_broadcasters/script.js', array(), true);
-
 JHtml::_('stylesheet', 'mod_broadcasters/style.css', array(), true);
-
-?>
- <ul class="pmbr">
- 	<?php foreach ($rows as $row): ?>
-	<li>
-		<span class="pmbr_zero"><?php echo $row[0]; ?></span><span class="pmbr_one"> <?php echo $row[1]; ?></span><span class="pmbr_two"><?php echo $row[2]; ?></span><span class="pmbr_three"><?php echo $row[3]; ?></span><span class="pmbr_four"><?php if (!empty($row[4])): ?>
-		com <?php echo $row[4]; ?></span>
-		<?php endif; ?>
-	</li> 
- 	<?php endforeach; ?>
- </ul>
-
+?>       
+<div class="pmbr"> 	                  
+  <?php foreach ($rows as $row): ?>      	                  
+  <div class="pmbr_li">                  
+    <div class="pmbr_one">                              
+      <?php if (!empty($row[0])): ?>                                          
+      <?php echo $row[0]; ?>      		                                      
+      <?php endif; ?><br /><hr>                                           
+      <?php if (!empty($row[1])): ?>                                               
+      <?php echo $row[1]; ?>      		                                      
+      <?php endif; ?>   &nbsp;                     
+      <?php echo JText::_( 'PMBR_HOURS' ); ?>    &nbsp;-&nbsp;                                              
+      <?php if (!empty($row[2])): ?>                                           
+      <?php echo $row[2]; ?>   &nbsp;                     
+      <?php echo JText::_( 'PMBR_HOURS' ); ?>    		                                      
+      <?php endif; ?>   	                      	                                   
+    </div>		                                         
+    <div class="pmbr_two">                                        
+      <?php if (!empty($row[3])): ?>                                           
+      <?php echo $row[3]; ?>      		                                      
+      <?php endif; ?><br /><hr>                             
+      <?php if (!empty($row[4])): ?>           
+      <?php echo $row[4]; ?>
+      <?php else: ?>
+       &nbsp;       		                                      
+      <?php endif; ?>      	                               
+    </div>                 
+  </div>   	                  
+  <?php endforeach; ?>              
+</div>
